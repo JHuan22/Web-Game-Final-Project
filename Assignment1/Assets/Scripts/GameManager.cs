6718044f5982c2d253af882b479fee296254b6f2
micheldaoud <michel_daoud@hotmail.com>
2023-02-04 21:38:29 -0500

    fix bugs lol

--- Assignment1/Assets/Scripts/GameManager.cs
+++ Assignment1/Assets/Scripts/GameManager.cs
@@ -10,7 +10,10 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Time.timeScale = 1;
         canvas.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        isPaused = false;
     }
 
     // Update is called once per frame
